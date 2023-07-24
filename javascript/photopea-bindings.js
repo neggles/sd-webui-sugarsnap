81b701d6733dcd46f567c80a956c0b23613d55af
ncpt <57245077+NoCrypt@users.noreply.github.com>
2023-07-24 08:13:45 +0700

    fix send_to buttons

--- javascript/photopea-bindings.js
+++ javascript/photopea-bindings.js
@@ -37,9 +37,9 @@ function onPhotopeaLoaded(e) {
   // Clone some buttons to send the contents of galleries in txt2img, img2img and extras tabs
   // to Photopea. You can also just copy-paste the images directly but these are the ones I
   // use the most.
-  createSendToPhotopeaButton("image_buttons_txt2img", window.txt2img_gallery);
-  createSendToPhotopeaButton("image_buttons_img2img", window.img2img_gallery);
-  createSendToPhotopeaButton("image_buttons_extras", window.extras_gallery);
+  createSendToPhotopeaButton("image_buttons_txt2img", window.txt2img_gallery[0]);
+  createSendToPhotopeaButton("image_buttons_img2img", window.img2img_gallery[0]);
+  createSendToPhotopeaButton("image_buttons_extras", window.extras_gallery[0]);
 
   // Listen to the size slider changes.
   gradioApp()
